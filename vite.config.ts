ae4069071ddb12494aa2aeac9ef4fd541efd59f3
laishengyuan <lsyuan.36@gmail.com>
2025-11-08 14:48:21 +0800

    [fix] network url path

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command }) => ({
+  base: '/Standing/',
+  plugins: [
+    vue(),
+    tsconfigPaths()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+}));
